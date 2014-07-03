4b58b2761b1151124c063ede8a5754d455b4de01
fxhover <736698959@qq.com>
2014-07-03 13:43:24 +0800

    routes update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,26 @@
+#encoding: utf-8
+require 'oj'
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+    return render_json(true, '欢迎使用')
+  end
+
+  private
+
+  def return_403_result(msg = '没有权限')
+    render status: 403, json: result(false, msg)
+  end
+
+  def result_json(status, data)
+    result = {status: status, result: data}
+    Oj.dump result
+  end
+
+  def render_json(status, data)
+    render status: 200, json: result_json(status, data)
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'multi_json'
+MultiJson.use :yajl
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,13 +13,26 @@ module Elva
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += %W(#{config.root}/lib)
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Beijing'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.default_locale = :"zh-CN"
+    config.i18n.available_locales = :"zh-CN"
+    config.encoding = "utf-8"
+
+    config.active_record.whitelist_attributes = true
+
   end
+end
+
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/elva.yml")
+enviroment_config_file = "#{Rails.root}/config/elva.#{Rails.env}.yml"
+if File.exists?(enviroment_config_file)
+  APP_CONFIG.merge!(YAML.load_file(enviroment_config_file))
 end
